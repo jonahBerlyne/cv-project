9232c08f792b0150ea83aee81c94c48d2210f9d1
jonahBerlyne <jonshaber@gmail.com>
2021-12-13 18:12:24 -0500

    613pm 12/13/21

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,11 +1,21 @@
+import React, { Component } from "react";
+
 // The Parent Component
 
-function App() {
-  return (
-    <div className="App">
-      Code goes here s'il vous plait
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Code goes here s'il vous plait!
+      </div>
+    );
+  }
+  
 }
 
 export default App;
